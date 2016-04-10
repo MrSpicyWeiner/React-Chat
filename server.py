53c27e2833271d0dfdb5acde5995f7ee86b80483
MrSpicyWeiner <Dubshelix@gmail.com>
2016-04-10 17:36:44 -0500

    Tons of fixes
    No longer constantly spams the network.
    No more console errors for not having a table around the rows
    Some spacing fixes

--- server.py
+++ server.py
@@ -12,25 +12,38 @@ import json
 import os
 import time
 from flask import Flask, Response, request
+import pusher
 
 app = Flask(__name__, static_url_path='', static_folder='public')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
+pusher = pusher.Pusher(
+  app_id='196661',
+  key='f829ca5282dc97707d03',
+  secret='147b477572416cbd712f'
+)
 @app.route('/api/comments', methods=['GET', 'POST'])
-def comments_handler():
+def new_message():
 
-    with open('comments.json', 'r') as file:
-        comments = json.loads(file.read())
+	with open('comments.json', 'r') as file:
+		comments = json.loads(file.read())
 
-    if request.method == 'POST':
-        newComment = request.form.to_dict()
-        newComment['id'] = int(time.time() * 1000)
-        comments.append(newComment)
+	if request.method == 'POST':
+		newComment = request.form.to_dict()
+		newComment['id'] = str(int(time.time() * 1000))
+		comments.append(newComment)
 
-        with open('comments.json', 'w') as file:
-            file.write(json.dumps(comments, indent=4, separators=(',', ': ')))
+		with open('comments.json', 'w') as file:
+			file.write(json.dumps(comments, indent=4, separators=(',', ': ')))
+			
+		pusher.trigger('messages', 'new_message', {
+			"author":newComment['author'],
+			"text":newComment['text'],
+			"id":newComment['id']
+		})
 
-    return Response(json.dumps(comments), mimetype='application/json', headers={'Cache-Control': 'no-cache', 'Access-Control-Allow-Origin': '*'})
+	return Response(json.dumps(comments), mimetype='application/json', headers={'Cache-Control': 'no-cache', 'Access-Control-Allow-Origin': '*'})
 
+	
 if __name__ == '__main__':
     app.run(host= '10.1.1.100',port=int(os.environ.get("PORT",3000)))
